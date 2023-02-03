b0cd7438a2c42240587b23c98c74347a72c5d74a
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2023-02-03 22:08:04 +0000

    Add JS

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const ms = core.getInput('terraform_docs_version');
+    core.info(`Waiting ${ms} milliseconds ...`);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
